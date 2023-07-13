2f6be90552ee49386e1c6839762cee9285fad21a
jesus miguel anguel pedraza mesa <jesusmipe@hotmail.com>
2023-07-12 23:23:35 -0500

    Commit proyecto

---
+++ app.js
@@ -0,0 +1,24 @@
+// tipos de datos
+"hello world" // string
+1000000
+10.1
+true //bolean
+false
+
+// array
+["JOE", "RYAN", "MARTHA"]
+
+// object
+"rian"
+70.4
+14
+true
+{
+    "úsername": "rian",
+        "score"; 70.4,
+            "hours": 14,
+                "proffesional": true
+    "friends"
+
+}
+console.log({ "username": "ryan", "score": 70.4 })
